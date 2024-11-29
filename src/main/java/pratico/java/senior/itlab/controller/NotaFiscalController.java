9499bc8434baa70bb3b093dec1e708156ee9a4a8
Arthur Paraiso <arthur.paraiso.martins7799@gmail.com>
2024-11-29 16:56:56 -0300

    Ajuste XML

--- src/main/java/pratico/java/senior/itlab/controller/NotaFiscalController.java
+++ src/main/java/pratico/java/senior/itlab/controller/NotaFiscalController.java
@@ -4,21 +4,29 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.kafka.core.KafkaTemplate;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import pratico.java.senior.itlab.model.NotaFiscal;
-import pratico.java.senior.itlab.service.NotaFiscalService;
 
-@RestController
 @AllArgsConstructor
-@RequestMapping("/notas")
+@RestController
 public class NotaFiscalController {
-    private NotaFiscalService service;
+    private KafkaTemplate<String, String> kafkaTemplate;
 
-    @PostMapping
-    public ResponseEntity<NotaFiscal> receberNota(@RequestBody NotaFiscal nota) {
-        NotaFiscal savedNota = service.processarNota(nota);
-        return ResponseEntity.ok(savedNota);
+    private static final String TOPIC = "nota_fiscal_topic";
+
+    @PostMapping("/notafiscal")
+    public ResponseEntity<String> sendNotaFiscal(@RequestBody NotaFiscal notaFiscal) {
+        try {
+            XmlMapper xmlMapper = new XmlMapper();
+            String xml = xmlMapper.writeValueAsString(notaFiscal);
+            kafkaTemplate.send(TOPIC, xml);
+            return ResponseEntity.ok("Nota Fiscal enviada com sucesso!");
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Erro ao enviar Nota Fiscal: " + e.getMessage());
+        }
     }
 }
+
 
